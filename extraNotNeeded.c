35f29e244bcd6b81066a0072148d4a6cae479d4e
BaileyMcCauslin <66763821+BaileyMcCauslin@users.noreply.github.com>
2022-05-02 23:14:40 -0700

    If you want expanding arrays consider this

---
+++ extraNotNeeded.c
@@ -0,0 +1,33 @@
+typedef struct PascalsTriangle
+    {
+      int size;
+  
+      int capacity;
+  
+      int *levelArray;
+    }PascalsTriangle;
+
+
+
+//expand the array when size hits capacity. Check for this before doing another level
+void expandArray( PascalsTriangle triangle )  //Note: Depending on the circumstance you might need to pass pointer
+{
+ PascalsTriangle newLevelArray;
+  
+ int index;
+  
+ if( triangle.size == triangle.capacity )
+ {
+  triangle.capacity*= 2;
+   
+  newLevelArray = ( int * )malloc( sizeof( int ) * triangle.capacity ); 
+   
+  for( index = 0; index < triangle.size; index++ )
+  {
+   newLevelArray[ index ] = triangle.levelArray[ index ];
+  }
+  free( triangle.levelArray );
+   
+  triangle.levelArray = newLevelArray;
+ }
+}
